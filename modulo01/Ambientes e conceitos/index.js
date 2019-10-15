e4c5e012e4b28f58f939bc13639906fa7fbc2a11
Paulo Henrique <phenriq694@gmail.com>
2019-10-15 13:18:42 -0300

    Correções com base na resposta

--- modulo01/Ambientes e conceitos/index.js
+++ modulo01/Ambientes e conceitos/index.js
@@ -4,64 +4,67 @@ const server = express()
 
 server.use(express.json())
 
-let count = 0
-
-server.use((req, res, next) => {
-  console.log(count += 1)
-
-  next()
-})
+let numberOfRequests = 0
+const projects = []
 
 function checkProjectExists(req, res, next) {
 
-  projects.filter(p => {
-    if(p.id == req.params.id) {
-      req.id = p.id
-    }
-  }) 
+  const { id } = req.params
 
-  if(!req.id) {
+  const project = projects.find(p => p.id == id) 
+
+  if(!project) {
     return res.status(400).json({ error: "Id does note exists"})
   }
+
+  req.id = project.id
 
   return next()
 }
 
-const projects = []
+function logRequests(req, res, next) {
+  numberOfRequests++
+  
+  console.log(`Número de requisições: ${numberOfRequests}`)
+
+  next()
+}
+
+server.use(logRequests)
 
 server.get('/projects', (req, res) => {
   res.json(projects)
 })
 
 server.post('/projects', (req, res) => {
-  const { id, title, task } = req.body 
+  const { id, title } = req.body 
 
-  const new_project = {id, title, task}
+  const new_project = {
+    id, 
+    title, 
+    task: []
+  }
 
   projects.push(new_project)
 
-  return res.json(projects)
+  return res.json(new_project)
 })
 
 server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { title } = req.body
 
-  projects.filter(project => {
-    if(project.id == req.id) {
-      project.title = title
-    }
-  })
+  const project = projects.find(project => project.id == req.id)
 
-  return res.json(projects)
+  project.title = title
+
+  return res.json(project)
 })
 
 server.delete('/projects/:id', checkProjectExists, (req, res) => {
 
-  projects.filter(project => {
-    if(project.id == req.id) {
-      projects.splice(projects.indexOf(project), 1)
-    }
-  })
+  const projectIndex = projects.findIndex(p => p.id == req.id)
+
+  projects.splice(projectIndex, 1)
 
   return res.json(projects)
 })
@@ -69,11 +72,9 @@ server.delete('/projects/:id', checkProjectExists, (req, res) => {
 server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
   const { title } = req.body
 
-  projects.filter(project => {
-    if(project.id == req.id) {
-      project.task.push(title)
-    }
-  })
+  const project = projects.find(project => project.id == req.id)
+
+  project.task.push(title)
 
   return res.json(projects)
 })
